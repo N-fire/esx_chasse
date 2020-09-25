e7d795323e66c01e39e67255ea25cc314ace40a4
N-fire <55892717+N-fire@users.noreply.github.com>
2020-09-25 18:42:41 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -5,9 +5,7 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 RegisterNetEvent('chasse:additem')
 
 AddEventHandler('chasse:additem', function(item, amount)
-    print("add item en cour")
     local _source = source
-    print(source)
     local xPlayer = ESX.GetPlayerFromId(_source)
     local item = item
     local amount = amount
